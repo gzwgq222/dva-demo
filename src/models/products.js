6cfcaa3a4e60253f3dcebe3f61106a84e6b37d94
gongqiang <gongqiang@xbonline.net>
2020-06-29 21:11:35 +0800

    init

---
+++ src/models/products.js
@@ -0,0 +1,10 @@
+// model
+export default {
+  namespace: "products",
+  state: [],
+  reducers: {
+    delete(state, { payload: id }) {
+      return state.filter((item) => item.id !== id);
+    },
+  },
+};
